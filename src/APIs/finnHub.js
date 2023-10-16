c8d8cd28173ac3a739ebecbbae6ede9de0102ea6
Irakoze Aime Aubin <irakozeaimeaubin@gmail.com>
2023-10-16 17:54:20 +0200

    fix site header

--- src/APIs/finnHub.js
+++ src/APIs/finnHub.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
-const API_KEY = 'cdq9s3qad3i5u3riklcgcdq9s3qad3i5u3rikld0'
+const API_KEY = 'cdq9s3qad3i5u3riklcgcdq9s3qad3i5u3rikld0';
 
 export default axios.create( {
     baseURL: 'https://finnhub.io/api/v1',
     params: {
         token: API_KEY
+    },
+    headers: {
+        "X-Finnhub-Token": API_KEY
     }
-})
+} );
